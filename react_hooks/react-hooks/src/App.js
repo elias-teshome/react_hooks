7eb5744189e06f7901806654279389ca9e7be14d
Elias Girma <eliasgirma@Eliass-MacBook-Pro.local>
2021-09-14 17:25:51 -0500

    useState hook using checkbox

--- react_hooks/react-hooks/src/App.js
+++ react_hooks/react-hooks/src/App.js
@@ -1,16 +1,21 @@
-
+import {useState} from 'react'
 import './App.css';
 
 
 function App() {
 
-  const  [el,,]=["elias","girma","teshome"]
+  //const  [el,,]=["elias","girma","teshome"]
+  
+ //const [state, setState] = useState("befor")
+ const [checked, setChecked]=useState(false);
 
   return (
-  <>
-  {console.log(el)
-  }
-  </>
+  <div>
+ 
+  
+  <input type='checkbox' value={checked} onChange={()=>setChecked(!checked)}/>
+  <p>{checked ? "Checked":"notchecked"}</p>
+  </div>
   );
 }
 
