60e12d3f08fae303f874f3bc451863ba09a9e931
Elias Girma <eliasgirma@Eliass-MacBook-Pro.local>
2021-09-14 17:54:48 -0500

    useState hook

--- react_hooks/react-hooks/src/App.js
+++ react_hooks/react-hooks/src/App.js
@@ -1,21 +1,35 @@
 import {useState} from 'react'
 import './App.css';
+import {FaStar} from 'react-icons/fa'
 
 
 function App() {
 
   //const  [el,,]=["elias","girma","teshome"]
   
- //const [state, setState] = useState("befor")
+ const [state, setState] = useState(true)
+
  const [checked, setChecked]=useState(false);
+const creatArary=(length)=>
+ [...Array(length)];
+
+function Star({selected})
+{
+  return <FaStar onClick={()=>setState(!state)} color={selected? 'red':'gray'}/>
+}
+function StarRating({total=5})
+{
+  return creatArary(total).map((n,i)=><Star selected={state} key={i}/>)
+}
 
   return (
+
   <div>
- 
-  
-  <input type='checkbox' value={checked} onChange={()=>setChecked(!checked)}/>
+ <input type='checkbox' value={checked} onChange={()=>setChecked(!checked)}/>
   <p>{checked ? "Checked":"notchecked"}</p>
+  <StarRating total={10}/>
   </div>
+
   );
 }
 
