15a20d612fa630261f50b4fb0f8a8909e0348e88
Elias Girma <eliasgirma@Eliass-MacBook-Pro.local>
2021-09-14 20:20:38 -0500

    fetching data with useEffect

--- react_hooks/react-hooks/src/App.js
+++ react_hooks/react-hooks/src/App.js
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 import './App.css';
 import {FaStar} from 'react-icons/fa'
 
@@ -22,15 +22,61 @@ function StarRating({total=5})
   return creatArary(total).map((n,i)=><Star selected={state} key={i}/>)
 }
 
+
+//use effect
+const [or, setOr]=useState("orGate");
+const [and, setAnd]=useState("andGate");
+
+useEffect(() => {
+ console.log(`the gate is ${or}`)
+},[or])
+
+useEffect(() => {
+  console.log(`the gate is ${and}`)
+ },[and])
+ 
+
+const [data,setDate]=useState([]);
+useEffect(()=>{
+  fetch('https://api.github.com/users')
+  .then(response=>response.json())
+  .then(r=>setDate(r));
+})
+
+
+
+if(data){
+
   return (
 
   <div>
  <input type='checkbox' value={checked} onChange={()=>setChecked(!checked)}/>
   <p>{checked ? "Checked":"notchecked"}</p>
   <StarRating total={10}/>
+
+  <button onClick={()=>setOr("or")}>or</button>
+  <button onClick={()=>setAnd("and")}>and</button>
+
+  <ul>
+      {
+       
+          data.map(d=>(
+          <li key={d.id}>
+
+            {d.login}
+
+          </li>))
+
+      }
+       
+     
+    </ul>
+
+
   </div>
 
   );
+}
 }
 
 export default App;
